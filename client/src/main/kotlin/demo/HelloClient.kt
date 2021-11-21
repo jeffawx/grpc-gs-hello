cccabddd7517463b7e24c03bfbb6c67b21047a72
Jeff Fang <jeff.fang@airwallex.com>
2021-11-21 15:07:37 +0800

    add logger

--- client/src/main/kotlin/demo/HelloClient.kt
+++ client/src/main/kotlin/demo/HelloClient.kt
@@ -2,6 +2,7 @@ package demo
 
 import com.airwallex.grpc.annotations.GrpcClient
 import kotlinx.coroutines.runBlocking
+import org.slf4j.LoggerFactory
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
@@ -9,8 +10,14 @@ import org.springframework.stereotype.Component
 @Component
 class HelloClient(@GrpcClient(channel = "hello") private val service: HelloRpc) {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     @EventListener
     fun welcome(e: ApplicationReadyEvent) = runBlocking {
+        logger.info("sending request to server")
+
         println(service.greet("World"))
+
+        logger.info("server responded")
     }
 }
--- server/src/main/kotlin/demo/HelloService.kt
+++ server/src/main/kotlin/demo/HelloService.kt
@@ -1,11 +1,16 @@
 package demo
 
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class HelloService : HelloRpc {
 
+    private val logger = LoggerFactory.getLogger(javaClass)
+
     override suspend fun greet(request: String): String {
+        logger.info("greet request received: {}", request)
+
         return "Hello, $request"
     }
 }
