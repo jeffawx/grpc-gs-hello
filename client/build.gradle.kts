71a7f303ccd4228ebf76da9b9de1d5426a367bf8
Jeff Fang <jeff.fang@airwallex.com>
2022-07-31 10:14:35 +0800

    upgrade to 1.3.0

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -1,3 +1,15 @@
 plugins {
-    id("com.airwallex.grpc-spring")
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    kotlin("jvm")
+    kotlin("plugin.spring")
+    id("com.airwallex.grpc")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("com.airwallex.grpc:grpc-spring-boot-starter")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
